9555e847fc5df5643a3dbd1d4a83324465c44bd2
Torsten Martinsen <torsten@bullestock.net>
2025-10-10 23:51:09 +0200

    Start adapting for using gears.

--- cad/hour.py
+++ cad/hour.py
@@ -2,6 +2,8 @@ from build123d import *
 from ocp_vscode import *
 from defs import *
 
+# Moving part for hours
+
 bearing_id = 20
 bearing_th = 4
 stem_h = 14.5
@@ -13,28 +15,31 @@ b_crush = 0.45
 h_crush = 0.2
 rr = 0.5
 
-p_dia = 65
-slot_dia = 2
-p_th = 4.5
+gear_inner_dia = 40
+gear_hole_cc = 30
+gear_hole_dia = 2.5
+gear_mount_th = 2
 
 bearing_clearance = 1
 
-bearing_z = p_th
+bearing_z = gear_mount_th + bearing_clearance
 
 bottom = (Align.CENTER, Align.CENTER, Align.MIN)
 
 with BuildPart() as p:
-    # pulley
-    Cylinder(p_dia/2, p_th)
-    Torus(p_dia/2, slot_dia/2, mode=Mode.SUBTRACT)
+    # gear mount with screw holes
+    Cylinder(gear_inner_dia/2, gear_mount_th)
+    with Locations(p.faces().sort_by(Axis.Z)[-1]):
+        with PolarLocations(radius=gear_hole_cc/2, count=4):
+            CounterSinkHole(gear_hole_dia/2, gear_hole_dia)
     # spacer
     with Locations([(0, 0, bearing_z/2)]):
-        Cylinder(bearing_id/2 + 0.5, bearing_clearance)
+        Cylinder(bearing_id/2 + 1.5, bearing_clearance)
     # part that fits inside bearing
     with Locations([(0, 0, bearing_z)]):
         Cylinder(bearing_id/2 - b_crush/2, bearing_th)
     # bearing crush ribs
-    with BuildSketch(p.faces().sort_by(Axis.Z)[0].offset(-p_th-rr)) as sk:
+    with BuildSketch(p.faces().sort_by(Axis.Z)[0].offset(-gear_mount_th - rr)) as sk:
         with PolarLocations(radius=bearing_id/2 - b_crush/2 - 0.1, count=10):
             Circle(b_crush)
     extrude(amount=-(bearing_th - 2*rr))
