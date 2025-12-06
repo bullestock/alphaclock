e54987f0146a1d12b229ddf1aee46a44906bc8cd
Torsten Martinsen <torsten@bullestock.net>
2025-12-06 09:52:18 +0100

    Tweaks.

--- cad/defs.py
+++ cad/defs.py
@@ -2,6 +2,7 @@ import math
 
 ow = 80
 insert_r = 4.1/2
+insert_h = 5
 motor_bracket_cc = 35
 screw_radius = math.cos(math.radians(45))*(ow - 10)
 bearing_id = 20
--- cad/secstepperholder.py
+++ cad/secstepperholder.py
@@ -6,18 +6,18 @@ from defs import *
 th = 5
 
 dx1 = 12
-dy1 = 70
-yo1 = 17.5
+dy1 = 67
+yo1 = 16
 dx2 = 12.5
-dy2 = 25
+dy2 = 22
 mmdx = 26
 mmdy = 15
 mount_h = 20
 hole_cc = 25
 hole_x = 0
 hole_y = 0
-motor_y = dy1/2 + 10
-motor_x = -18
+motor_y = 42
+motor_x = -17
 motor_z = 13
 mot_d = 19.7
 
@@ -26,7 +26,7 @@ with BuildPart() as p:
     with BuildSketch(Plane.XY):
         with Locations((0, yo1)):
             RectangleRounded(dx1, dy1, 4)
-        with Locations((-4, 40)):
+        with Locations((-3, 38.5)):
             RectangleRounded(dx2, dy2, 2)
     extrude(amount=th)
     # screw holes
